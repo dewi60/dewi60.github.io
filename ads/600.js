a0d384fc1e35ee8b9f924bbea6337a5d5d1068d5
dewi60 <111417487+dewi60@users.noreply.github.com>
2023-01-04 09:10:31 +0700

    Create 600.js

---
+++ ads/600.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '330bd1a571a50dbbe00069becfd6e7a1',
+		'format' : 'iframe',
+		'height' : 600,
+		'width' : 160,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://maggotpolity.com/330bd1a571a50dbbe00069becfd6e7a1/invoke.js"></scr' + 'ipt>');
