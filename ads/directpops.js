64db98378a2c6beef974828c0242f5ea026b24a8
dewi60 <111417487+dewi60@users.noreply.github.com>
2023-02-05 08:55:32 +0700

    Create directpops.js

---
+++ ads/directpops.js
@@ -0,0 +1,10 @@
+$(document).ready(function()
+ {
+   $('body').addClass('xepo_ads');
+ });
+ $(document).on('click','.xepo_ads',function(e)
+ {
+     $(this).removeClass('xepo_ads');
+     //ubah google dengan ads direct link
+     window.open('https://maggotpolity.com/fy15whpk?key=9d7aa7088a7cd82f76abed276e814809', '_blank');
+ });
