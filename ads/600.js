c69f18d561b988a40cfaebe16340a02caea65d40
dewi60 <111417487+dewi60@users.noreply.github.com>
2023-02-28 16:40:01 +0700

    Update 600.js

--- ads/600.js
+++ ads/600.js
@@ -11,7 +11,7 @@ if (window.jstiming) window.jstiming.load.tick('headEnd');
 <div id="teaser2" style="width:autopx; height:0; text-align:center; display:scroll;position:fixed; top:0px;center:0px;">
 <div><a id="close-teaser" onclick="document.getElementById('teaser2').style.display = 'none';" style="cursor:pointer;"><center><img src='https://4.bp.blogspot.com/-9MWyoN5VsJM/TivTpPyUuhI/AAAAAAAABL0/ldO739MTRBg/s1600/close3.png' alt='close' title='close button'/></center></a></div>
 <!--Mulai disini-->
-<script type="text/javascript">
+
 	atOptions = {
 		'key' : 'c7cc9bbe583b37fca4b40d77af3687bf',
 		'format' : 'iframe',
@@ -20,7 +20,7 @@ if (window.jstiming) window.jstiming.load.tick('headEnd');
 		'params' : {}
 	};
 	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://maggotpolity.com/c7cc9bbe583b37fca4b40d77af3687bf/invoke.js"></scr' + 'ipt>');
-</script>
+
 <!--Akhir disini-->
 </div>
 <!--end: floating ads-->
