b3c323b4c94a662653d79ffcbd0a63d7cacd8211
dewi60 <111417487+dewi60@users.noreply.github.com>
2023-02-28 16:35:46 +0700

    Update 600.js

--- ads/600.js
+++ ads/600.js
@@ -1,4 +1,19 @@
-atOptions = {
+$(document).ready(function() {
+$('img#closed').click(function(){
+$('#btm_banner').hide(90);
+});
+});
+
+<script type="text/javascript">var a=navigator,b="userAgent",c="indexOf",f="&m=1",g="(^|&)m=",h="?",i="?m=1";function j(){var d=window.location.href,e=d.split(h);switch(e.length){case 1:return d+i;case 2:return 0<=e[1].search(g)?null:d+f;default:return null}}if(-1!=a[b][c]("Mobile")&&-1!=a[b][c]("WebKit")&&-1==a[b][c]("iPad")||-1!=a[b][c]("Opera Mini")||-1!=a[b][c]("IEMobile")){var k=j();k&&window.location.replace(k)};
+</script><script type="text/javascript">
+if (window.jstiming) window.jstiming.load.tick('headEnd');
+</script>
+<!--start: floating ads-->
+<div id="teaser2" style="width:autopx; height:0; text-align:center; display:scroll;position:fixed; top:0px;center:0px;">
+<div><a id="close-teaser" onclick="document.getElementById('teaser2').style.display = 'none';" style="cursor:pointer;"><center><img src='https://4.bp.blogspot.com/-9MWyoN5VsJM/TivTpPyUuhI/AAAAAAAABL0/ldO739MTRBg/s1600/close3.png' alt='close' title='close button'/></center></a></div>
+<!--Mulai disini-->
+<script type="text/javascript">
+	atOptions = {
 		'key' : 'c7cc9bbe583b37fca4b40d77af3687bf',
 		'format' : 'iframe',
 		'height' : 250,
@@ -6,3 +21,7 @@ atOptions = {
 		'params' : {}
 	};
 	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://maggotpolity.com/c7cc9bbe583b37fca4b40d77af3687bf/invoke.js"></scr' + 'ipt>');
+</script>
+<!--Akhir disini-->
+</div>
+<!--end: floating ads-->
