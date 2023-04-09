f974bbe8eeff747a83d2cef0eea66480e7958654
dewi60 <111417487+dewi60@users.noreply.github.com>
2023-04-09 10:06:11 +0700

    Update and rename 250sz.js to 250s.js

---
+++ ads/250s.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : 'f0441d659261d39c05d117d8e29c08b5',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://maggotpolity.com/f0441d659261d39c05d117d8e29c08b5/invoke.js"></scr' + 'ipt>');
--- ads/250sz.js
+++
@@ -1,8 +0,0 @@
-atOptions = {
-		'key' : 'a49efb2fff696516c383c9b218756eef',
-		'format' : 'iframe',
-		'height' : 250,
-		'width' : 300,
-		'params' : {}
-	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://maggotpolity.com/a49efb2fff696516c383c9b218756eef/invoke.js"></scr' + 'ipt>');
