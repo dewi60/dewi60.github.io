172dc38b8a1298eb03e0110078241f7d4eeadc04
dewi60 <111417487+dewi60@users.noreply.github.com>
2023-04-10 09:45:48 +0700

    Update 250s.js

--- ads/250s.js
+++ ads/250s.js
@@ -1,8 +1,8 @@
 atOptions = {
-		'key' : 'f0441d659261d39c05d117d8e29c08b5',
+		'key' : 'a49efb2fff696516c383c9b218756eef',
 		'format' : 'iframe',
 		'height' : 250,
 		'width' : 300,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://maggotpolity.com/f0441d659261d39c05d117d8e29c08b5/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://maggotpolity.com/a49efb2fff696516c383c9b218756eef/invoke.js"></scr' + 'ipt>');
