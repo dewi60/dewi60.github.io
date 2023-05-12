0c24699ef3df4d1b905f0eb8a0e2150c0da33fee
dewi60 <111417487+dewi60@users.noreply.github.com>
2023-05-12 14:23:39 +0700

    Create book250.js

---
+++ ads/book250.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '925c797a50c4f56dca1844ddce33e33f',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://maggotpolity.com/925c797a50c4f56dca1844ddce33e33f/invoke.js"></scr' + 'ipt>');
