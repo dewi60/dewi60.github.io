bc665c3016d1f31c10437ad7dec70d09e01f91f4
dewi60 <111417487+dewi60@users.noreply.github.com>
2024-05-22 10:26:04 +0700

    Update book250.js

--- ads/book250.js
+++ ads/book250.js
@@ -5,4 +5,4 @@ atOptions = {
 		'width' : 300,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://maggotpolity.com/925c797a50c4f56dca1844ddce33e33f/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://furyprosecutionkitchen.com/925c797a50c4f56dca1844ddce33e33f/invoke.js"></scr' + 'ipt>');
