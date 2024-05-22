ec41f93cbf8ca0a55acbfba08143c8eba9effd74
dewi60 <111417487+dewi60@users.noreply.github.com>
2024-05-22 10:25:28 +0700

    Update 728z.js

--- ads/728z.js
+++ ads/728z.js
@@ -5,4 +5,4 @@ atOptions = {
 		'width' : 728,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://maggotpolity.com/30890a410d321c8eb0727444a2bd807c/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://furyprosecutionkitchen.com/30890a410d321c8eb0727444a2bd807c/invoke.js"></scr' + 'ipt>');
