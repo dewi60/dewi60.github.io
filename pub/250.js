92fd4fd27f3f3cc14bd468d04b2ae77cb004d60b
dewi60 <111417487+dewi60@users.noreply.github.com>
2025-07-08 19:20:32 +0700

    Update 250.js

--- pub/250.js
+++ pub/250.js
@@ -5,4 +5,4 @@ atOptions = {
 		'width' : 300,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/8c51c257125a052c08eb44ec88cd596c/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highperformanceformat.com/8c51c257125a052c08eb44ec88cd596c/invoke.js"></scr' + 'ipt>');
