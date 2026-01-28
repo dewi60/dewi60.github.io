5f59dff27fb88a2c6da6be3949ce6beb7246d52e
dewi60 <111417487+dewi60@users.noreply.github.com>
2026-01-28 12:18:55 +0700

    Update book250.js

--- ads/book250.js
+++ ads/book250.js
@@ -5,4 +5,4 @@ atOptions = {
 		'width' : 300,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://enraptureminims.com/925c797a50c4f56dca1844ddce33e33f/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://frozenbiopsyskate.com/a49efb2fff696516c383c9b218756eef/invoke.js"></scr' + 'ipt>');
