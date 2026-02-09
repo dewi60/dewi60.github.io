e95169706cc0189a43f8304b1ea3c4999273da2e
dewi60 <111417487+dewi60@users.noreply.github.com>
2026-02-09 19:36:50 +0700

    Update movies250.js

--- ads/movies250.js
+++ ads/movies250.js
@@ -1,8 +1,8 @@
 atOptions = {
-		'key' : 'b126943beef3cc5eddb47de8fb70b5d2',
+		'key' : 'a49efb2fff696516c383c9b218756eef',
 		'format' : 'iframe',
 		'height' : 250,
 		'width' : 300,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://frozenbiopsyskate.com/b126943beef3cc5eddb47de8fb70b5d2/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://frozenbiopsyskate.com/a49efb2fff696516c383c9b218756eef/invoke.js"></scr' + 'ipt>');
